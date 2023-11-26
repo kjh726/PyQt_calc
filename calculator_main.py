3851dfcad3235fb8ee4f47a515283e306a04d606
kjh726 <149755364+kjh726@users.noreply.github.com>
2023-11-26 13:40:34 +0900

    issue #3 (새 버튼 추가)
    issue #3
    새 버튼 추가
    (새 버튼 추가로 인한 배치 변경 및 숫자 배치 변경)
    (동작은 안됨. 추가와 배치 변경만.)

--- calculator_main.py
+++ calculator_main.py
@@ -19,6 +19,8 @@ class Main(QDialog):
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
         layout_equation_solution.addWidget(self.equation_solution)
+
+        ## 버튼 생성 및 시그널 설정(숫자 제외)
         
         ### 사칙연산 버튼 생성
         button_plus = QPushButton("+")
@@ -32,11 +34,6 @@ class Main(QDialog):
         button_product.clicked.connect(lambda state, operation = "*": self.button_operation_clicked(operation))
         button_division.clicked.connect(lambda state, operation = "/": self.button_operation_clicked(operation))
 
-        ### 사칙연산 버튼을 layout_operation 레이아웃에 추가
-        basic_operations = [button_division, button_product, button_minus, button_plus]
-        for i in range(4):
-            layout_button.addWidget(basic_operations[i], i, 3)
-
         ### =, clear, backspace 버튼 생성 + ce 버튼 생성
         button_equal = QPushButton("=")
         button_clear = QPushButton("C")
@@ -49,12 +46,32 @@ class Main(QDialog):
         button_backspace.clicked.connect(self.button_backspace_clicked)
         button_ce.clicked.connect(self.button_clear_clicked)
 
-        ### =, clear, backspace 버튼을 layout_button 레이아웃에 추가 + ce 버튼 layout_button 레이아웃에 추가
-        layout_button.addWidget(button_clear, 0, 1)
-        layout_button.addWidget(button_backspace, 0, 2)
-        layout_button.addWidget(button_equal, 4, 3)
-        layout_button.addWidget(button_ce, 0, 0)
+        ### 추가 버튼 생성
+        button_mod = QPushButton("%")
+        button_rv = QPushButton("1/x")
+        button_sq = QPushButton("x²")
+        button_rt = QPushButton("√x")
 
+        ### 추가 버튼 클릭 시 시그널 설정
+        button_mod.clicked.connect(lambda state, operation = "%":self.button_operation_clicked(operation))
+        button_rv.clicked.connect(lambda state, operation = "**(-1)":self.button_operation_clicked(operation))
+        button_sq.clicked.connect(lambda state, operation = "**2":self.button_operation_clicked(operation))
+        button_rt.clicked.connect(lambda state, operation = "**(1/2)":self.button_operation_clicked(operation))
+
+        ## 레이아웃에 추가(숫자 제외)
+        
+        ### 오른쪽 사이드의 버튼들을 레이아웃에 추가
+        right_side_button = [button_backspace, button_division, button_product, button_minus, button_plus, button_equal]
+        for i in range(6):
+            layout_button.addWidget(right_side_button[i], i, 3)
+
+        ### 나머지 버튼들을 레이아웃에 추가
+        others = [[button_mod, button_ce, button_clear], [button_rv, button_sq, button_rt]]
+        for i in range(2):
+            for j in range(3):
+                layout_button.addWidget(others[i][j], i, j)
+
+        
         ### 숫자 버튼 생성하고, layout_button 레이아웃에 추가
         ### 각 숫자 버튼을 클릭했을 때, 숫자가 수식창에 입력 될 수 있도록 시그널 설정
         number_button_dict = {}
@@ -63,23 +80,23 @@ class Main(QDialog):
             number_button_dict[number].clicked.connect(lambda state, num = number:
                                                        self.number_button_clicked(num))
             if number >0:
-                x,y = divmod(number-1, 3)
-                layout_button.addWidget(number_button_dict[number], x+1, y)
+                x,y = divmod(15-number, 3)
+                layout_button.addWidget(number_button_dict[number], x, abs(2-y))
             elif number==0:
-                layout_button.addWidget(number_button_dict[number], 4, 1)
+                layout_button.addWidget(number_button_dict[number], 5, 1)
 
         ### 소숫점 버튼과 00 버튼을 입력하고 시그널 설정
         button_dot = QPushButton(".")
         button_dot.clicked.connect(lambda state, num = ".": self.number_button_clicked(num))
-        layout_button.addWidget(button_dot, 4, 2)
+        layout_button.addWidget(button_dot, 5, 2)
 
         button_double_zero = QPushButton("00")
         button_double_zero.clicked.connect(lambda state, num = "00": self.number_button_clicked(num))
-        layout_button.addWidget(button_double_zero, 4, 0)
+        layout_button.addWidget(button_double_zero, 5, 0)
 
         ### 각 레이아웃을 main_layout 레이아웃에 추가(**행, 열, 행크기, 열크기)
         main_layout.addLayout(layout_equation_solution, 0, 0, 1, 4)
-        main_layout.addLayout(layout_button, 1, 0, 5, 4)
+        main_layout.addLayout(layout_button, 1, 0, 6, 4)
 
         self.setLayout(main_layout)
         self.show()
